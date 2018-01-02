5a9108dabe8a7202e899c9094bd6f6dfb9ec4661
iambedant_kuliza <bebi2527@live.in>
2018-01-02 20:58:51 +0530

    feat: Initial commit

---
+++ app/src/main/java/com/iambedant/tensoreflowlite/AutoAdjustableTextureView.kt
@@ -0,0 +1,48 @@
+package com.iambedant.tensoreflowlite
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.TextureView
+import android.view.View
+
+/**
+ * Created by @iamBedant on 01/01/18.
+ */
+class AutoAdjustableTextureView: TextureView{
+
+    private var mRatioWidth = 0
+    private var mRatioHeight = 0
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+
+    fun setAspectRatio(width: Int, height: Int) {
+        if (width < 0 || height < 0) {
+            throw IllegalArgumentException("Size cannot be negative.")
+        }
+        mRatioWidth = width
+        mRatioHeight = height
+        requestLayout()
+    }
+
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        val width = View.MeasureSpec.getSize(widthMeasureSpec)
+        val height = View.MeasureSpec.getSize(heightMeasureSpec)
+
+        if (0 == mRatioWidth || 0 == mRatioHeight) {
+            setMeasuredDimension(width, height)
+        } else {
+            if (width < height * mRatioWidth / mRatioHeight) {
+                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth)
+            } else {
+                setMeasuredDimension(height * mRatioWidth / mRatioHeight, height)
+            }
+        }
+    }
+
+}
